5bf594c0c358179d9126f5556af61aa652054672
Charlie <19439935+charliepja@users.noreply.github.com>
2020-03-05 22:10:14 +0000

    Adds tests for day2

--- src/test/java/DayTwoTest.java
+++ src/test/java/DayTwoTest.java
@@ -1,2 +1,44 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class DayTwoTest {
+    DayTwo dayTwo;
+    ArrayList<Integer> result;
+
+    @Before
+    public void before() {
+        dayTwo = new DayTwo();
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        dayTwo.setNumbers(numbers);
+        result = new ArrayList<Integer>();
+        result.add(120);
+        result.add(60);
+        result.add(40);
+        result.add(30);
+        result.add(24);
+    }
+
+    @Test
+    public void doesNotRunWhenEmptyArrayList() {
+        assertEquals("No numbers", dayTwo.productOfArray());
+    }
+
+    @Test
+    public void doesNotRunWhenArrayListSizeLessThanThree() {
+        assertEquals("Not enough numbers", dayTwo.productOfArray());
+    }
+
+    @Test
+    public void returnsArrayListOfProductOfOriginalArrayList() {
+        assertEquals(result, dayTwo.productOfArray());
+    }
 }
